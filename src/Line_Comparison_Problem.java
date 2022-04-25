86806221d121e13e365b6985b15c50f2bea5c840
SSK137 <saurabhkodam@gmail.com>
2022-04-25 13:12:56 +0530

    LineComparison : Length Comparison Equals

--- src/Line_Comparison_Problem.java
+++ src/Line_Comparison_Problem.java
@@ -1,8 +1,19 @@
 import java.util.*;
 public class Line_Comparison_Problem {
+    static void equals(Integer lenght1, Integer lenght2){
+        boolean result = lenght1.equals(lenght2);
+
+        if (result ==true)
+            System.out.println("Length of lines is Equal");
+        else
+            System.out.println("Lines are not equals...");
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        //accepting co-ordinates for line one
         System.out.println("Enter co-ordinates  For X1");
         int x1 = sc.nextInt();
         System.out.println("Enter co-ordinates  For Y1");
@@ -11,10 +22,12 @@ public class Line_Comparison_Problem {
         int x2 = sc.nextInt();
         System.out.println("Enter co-ordinates  For Y1");
         int y2 = sc.nextInt();
+
+        //calculating line 1
         Integer length1 = (int) Math.sqrt((x2 - x1) * 2 + (y2 - y1) * 2);
 
         System.out.println("\n");
-
+        //Accepting co-ordinates for line 2
         System.out.println("Enter co-ordinates  For A1");
         int a1 = sc.nextInt();
         System.out.println("Enter co-ordinates  For B1");
@@ -23,10 +36,14 @@ public class Line_Comparison_Problem {
         int a2 = sc.nextInt();
         System.out.println("Enter co-ordinates For B2");
         int b2 = sc.nextInt();
-
+        //calculating Line 2
         Integer length2 = (int) Math.sqrt((a2 - a1) * 2 + (b2 - a1) * 2);
 
+        //printing Both lines
         System.out.println("Length of 1st line is : " + length1);
         System.out.println("Length of second line is : " + length2);
+
+        //Calling Equal Method
+        equals(length1, length2);
     }
 }
