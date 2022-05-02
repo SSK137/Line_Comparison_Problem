33217d8b4fe45d143554a9be64302d5b4eca8427
SSK137 <saurabhkodam@gmail.com>
2022-05-02 10:21:05 +0530

    Line Comparison : Using CompareTo method

--- src/Line_Comparison_Problem.java
+++ src/Line_Comparison_Problem.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class Line_Comparison_Problem {
-    static void equals(Integer lenght1, Integer lenght2){
+    static void equalsTo(Integer lenght1, Integer lenght2){
         boolean result = lenght1.equals(lenght2);
 
-        if (result ==true)
+        if ( result == true )
             System.out.println("Length of lines is Equal");
         else
             System.out.println("Lines are not equals...");
     }
     //Method For Comparing Line
-    static void compare(Integer length1,Integer length2){
+    static void compareTo(Integer length1,Integer length2){
         int result = length1.compareTo(length2);
         if(result>0)
             System.out.println("Line One is greater :");
@@ -33,12 +33,11 @@ public class Line_Comparison_Problem {
         System.out.println("Enter co-ordinates  For Y1");
         int y2 = sc.nextInt();
 
-        //calculating line 1
+        //calculating length 1
         Integer length1 = (int) Math.sqrt((x2 - x1) * 2 + (y2 - y1) * 2);
 
-        System.out.println("\n");
         //Accepting co-ordinates for line 2
-        System.out.println("Enter co-ordinates  For A1");
+        System.out.println("\nEnter co-ordinates  For A1");
         int a1 = sc.nextInt();
         System.out.println("Enter co-ordinates  For B1");
         int b1 = sc.nextInt();
@@ -46,17 +45,15 @@ public class Line_Comparison_Problem {
         int a2 = sc.nextInt();
         System.out.println("Enter co-ordinates For B2");
         int b2 = sc.nextInt();
-        //calculating Line 2
-        Integer length2 = (int) Math.sqrt((a2 - a1) * 2 + (b2 - a1) * 2);
+        //calculating Length 2
+        Integer length2 = (int) Math.sqrt((a2 - a1) * 2 + (b2 - b1) * 2);
 
-        //printing Both lines
+        //print length 1 and length 2
         System.out.println("Length of 1st line is : " + length1);
         System.out.println("Length of second line is : " + length2);
 
-        //Calling Equal Method
-        equals(length1, length2);
-        //calling Compare Method
-        compare(length1, length2);
+        equalsTo(length1, length2);
+        compareTo(length1, length2);
     }
 
 }
